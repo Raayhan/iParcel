60eba9784416e1f8034bb04aa50e1cc907a1ab5e
Raayhan <rayhan.rakib@northsouth.edu>
2020-09-13 21:22:50 +0600

    Added Customers login panel

--- resources/views/customer/login.blade.php
+++ resources/views/customer/login.blade.php
@@ -4,15 +4,15 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card raleway">
+                <div class="card-header text-center font-weight-bold"><i class="fas fa-lock"></i> &nbsp;{{ __('Customer Login') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="padding-top:10%;padding-bottom:10%;">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -51,20 +51,33 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-4">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-unique MyButton">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                    <a class="btn btn-link font-weight-bold" href="{{ route('password.request') }}"><small>
+                                        {{ __('Forgot Your Password?') }}</small>
                                     </a>
                                 @endif
                             </div>
                         </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-sm-4 offset-sm-4">
+                              <a class="btn btn-sm text-dark btn-outline-primary waves-effect font-weight-bold" href="#"><img src="https://img.icons8.com/color/16/000000/google-logo.png"> &nbsp;&nbsp;Sign in with Google&nbsp;&nbsp;</a> 
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-sm-4 offset-sm-4">
+                                <a class="btn btn-sm text-dark btn-outline-primary waves-effect font-weight-bold" href="#"><img src="https://img.icons8.com/color/16/000000/facebook-new.png"> Sign in with Facebook</a> 
+                              </div>
+                            
+                        </div>
+                       
                     </form>
+                   
                 </div>
             </div>
         </div>
